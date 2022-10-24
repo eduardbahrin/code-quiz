8a081e5df5b190e7bbdc4e3dcb977c45b8c329f6
EduardB <eduard.bahrin@yahoo.com>
2022-10-24 23:50:19 +0100

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var questionsSource = [
+  {
+    question: "Question 1 : Commonly used data types DO NOT include:",
+    choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+    answer: "c",
+  },
+  {
+    question:
+      "Question 2 : The condition in an if / else statement is enclosed within ____.",
+    choices: [
+      "a. quotes",
+      "b. curly brackets",
+      "c. parentheses",
+      "d. square brackets",
+    ],
+    answer: "c",
+  },
+  {
+    question: "Question 3 : Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "a. numbers and strings",
+      "b. other arrays",
+      "c. booleans",
+      "d. all of the above",
+    ],
+    answer: "d",
+  },
+  {
+    question:
+      "Question 4 : String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parentheses"],
+    answer: "c",
+  },
+  {
+    question:
+      "Question 5 : A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "a. JavaScript",
+      "b. terminal / bash",
+      "c. for loops",
+      "d. console.log",
+    ],
+    answer: "d",
+  },
+  {
+    question: "Question 6 : The first index of an array is ____.",
+    choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+    answer: "a",
+  },
+  {
+    question: "Question 7 : What does DOM stand for?",
+    choices: [
+      "a. Document Object Model",
+      "b. Do Overnight Modules",
+      "c. Divas Obviously Model",
+      "d. Description, Operation & Maintenance",
+    ],
+    answer: "a",
+  },
+  {
+    question:
+      "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+    choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+    answer: "a",
+  },
+];
